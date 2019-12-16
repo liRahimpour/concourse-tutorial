d7ae25d607c721291a5755ba5a0c5aab75d81465
Ali Rahimpour <38255083+liRahimpour@users.noreply.github.com>
2019-12-16 13:17:26 +0100

    2

--- tutorials/basic/task-outputs-to-inputs/create_some_files.sh
+++ tutorials/basic/task-outputs-to-inputs/create_some_files.sh
@@ -7,3 +7,8 @@ echo "file3" > some-files/file3
 echo "file4" > some-files/file4
 
 ls some-files/*
+
+echo kiiiirrr
+
+
+echo fertig
--- tutorials/basic/task-outputs-to-inputs/show_files.sh
+++ tutorials/basic/task-outputs-to-inputs/show_files.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 ls some-files/*
+
+echo khubeye akhare inja kiram dahanet
+
+sleep 5m
+
+echo va tamam
